47f829fb9f390738f19548f5472dbf7ab7ee77f7
Daniel Mino <dannymfdz96@gmail.com>
2024-04-09 19:08:54 -0600

    Fix 3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,6 +28,19 @@ async function loadFonts() {
 }
 
 /**
+ * Builds all synthetic blocks in a container element.
+ * @param {Element} main The container element
+ */
+function buildAutoBlocks(main) {
+  try {
+    buildHeroBlock(main);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Auto Blocking failed', error);
+  }
+}
+
+/**
  * Decorates the main element.
  * @param {Element} main The main element
  */
